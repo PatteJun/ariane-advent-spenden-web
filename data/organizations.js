abb6ba914f2090b847157d8560ab717823b1f8d5
Christoph Paterok <christophpaterok@Laptopvhristoph.fritz.box>
2024-11-30 15:28:59 +0100

    update

---
+++ data/organizations.js
@@ -0,0 +1,194 @@
+export const organizations = [
+    {
+        "Tag": "11",
+        "Keyword": "MAMA",
+        "Name": "Straßenwächter e.V.",
+        "Beschreibung": "Engagiert sich seit 2005 für Obdachlose in Köln durch tägliche Touren mit Versorgung und Unterstützung.",
+        "Webseite": "strassenwaechter.de",
+        "Nachricht": "Mama, Du hast mir beigebracht, dass man Menschen in Not immer helfen sollte. ❤️"
+    },
+    {
+        "Tag": "10",
+        "Keyword": "HOLZTISCH",
+        "Name": "Arche für Obdachlose e.V.",
+        "Beschreibung": "Unterstützt Obdachlose in Köln und Umgebung mit verschiedenen Hilfsangeboten.",
+        "Webseite": "arche-obdach.org",
+        "Nachricht": "An unserem Holztisch haben wir schon so viele schöne Momente erlebt. Andere Menschen haben nicht mal einen Tisch."
+    },
+    {
+        "Tag": "14",
+        "Keyword": "TEAM",
+        "Name": "Helping Hands Cologne e.V.",
+        "Beschreibung": "Seit 2014 aktiv in der Unterstützung von Obdachlosen und Bedürftigen in Köln.",
+        "Webseite": "hhc-obdachlosenhilfe.koeln",
+        "Nachricht": "Wir sind das beste Team! 💑"
+    },
+    {
+        "Tag": "8",
+        "Keyword": "WASSERRATTE",
+        "Name": "OASE Benedikt Labre e.V.",
+        "Beschreibung": "Bietet seit über 30 Jahren Beratung und Hilfe für Wohnungslose in Köln.",
+        "Webseite": "oase-koeln.de",
+        "Nachricht": "Meine kleine Wasserratte, Du liebst das Meer - leider haben nicht alle Menschen Zugang zu sauberem Wasser."
+    },
+    {
+        "Tag": "20",
+        "Keyword": "SCHELM",
+        "Name": "SKM Köln – Obdach- und Wohnungslosenhilfe",
+        "Beschreibung": "Bietet vielfältige Hilfen für obdach- und wohnungslose Menschen in Köln.",
+        "Webseite": "skm-koeln.de",
+        "Nachricht": "Du kleiner Schelm bringst mich jeden Tag zum Lachen! 😊"
+    },
+    {
+        "Tag": "18",
+        "Keyword": "KÜSSCHEN",
+        "Name": "Sozialdienst katholischer Frauen e.V. Köln",
+        "Beschreibung": "Unterstützt obdachlose Frauen und Familien mit verschiedenen Angeboten.",
+        "Webseite": "skf-koeln.de",
+        "Nachricht": "Ein Küsschen für die beste Frau der Welt! 😘"
+    },
+    {
+        "Tag": "2",
+        "Keyword": "LÖWE",
+        "Name": "Animal Rescue Algarve (ARA)",
+        "Beschreibung": "Rettet und rehabilitiert verlassene und misshandelte Tiere in der Algarve, Portugal.",
+        "Webseite": "animalrescuealgarve.com",
+        "Nachricht": "Stark wie ein Löwe - das bist Du! 🦁"
+    },
+    {
+        "Tag": "3",
+        "Keyword": "COFFEEGANG",
+        "Name": "Tierrettung Portugal e.V.",
+        "Beschreibung": "Vermittelt Hunde aus Tötungsstationen und Tierheimen in Portugal.",
+        "Webseite": "tierrettung-portugal.de",
+        "Nachricht": "Coffee & Du - die beste Gang der Welt! ☕"
+    },
+    {
+        "Tag": "1",
+        "Keyword": "RUDI",
+        "Name": "Dogs of Portugal",
+        "Beschreibung": "Vermittelt Hunde aus einem Tierheim in Portugal nach Deutschland.",
+        "Webseite": "dogsofportugal.de",
+        "Nachricht": "Für unseren Rudi und alle anderen Hunde, die noch ein Zuhause suchen. 🐕"
+    },
+    {
+        "Tag": "4",
+        "Keyword": "BEACHBOY",
+        "Name": "Tierschutz mit Herz e.V.",
+        "Beschreibung": "Gemeinnütziger Verein im Raum Köln/Hennef, der Hunde zur Adoption vermittelt.",
+        "Webseite": "tierschutzmitherz.de",
+        "Nachricht": "Mit Dir am Strand - das pure Glück! 🏖️"
+    },
+    {
+        "Tag": "15",
+        "Keyword": "BOYS",
+        "Name": "Kinderheim Kleine Strolche e.V.",
+        "Beschreibung": "Freier Träger der Kinder- und Jugendhilfe mit mehreren Standorten in Niedersachsen, spezialisiert auf die Inobhutnahme von Säuglingen und Kleinkindern.",
+        "Webseite": "kinderheim-kleine-strolche.de",
+        "Nachricht": "Unsere Boys sind unser größtes Glück! 👶👶"
+    },
+    {
+        "Tag": "12",
+        "Keyword": "PARTYTIGER",
+        "Name": "EBM International",
+        "Beschreibung": "Bietet Schutz und Heimat für verwahrloste, verlassene und missbrauchte Kinder, die ohne Hilfe chancenlos wären.",
+        "Webseite": "ebm-international.org/de/",
+        "Nachricht": "Mit Dir zu tanzen ist das Beste! 🎉"
+    },
+    {
+        "Tag": "7",
+        "Keyword": "SCHMIDTIS",
+        "Name": "Afghanischer Frauenverein e. V.",
+        "Beschreibung": "Unterstützt Frauen und Kinder in Afghanistan durch Bildungs-, Gesundheits- und Infrastrukturprojekte.",
+        "Webseite": "afghanischer-frauenverein.de",
+        "Nachricht": "Familie Schmidt - zusammen unschlagbar! 👨‍👩‍👦‍👦"
+    },
+    {
+        "Tag": "24",
+        "Keyword": "KUSCHELTIGER",
+        "Name": "Aktion Canchanabury e.V.",
+        "Beschreibung": "Setzt sich für Gesundheitsprojekte in Afrika ein, insbesondere für HIV/AIDS-Betroffene und benachteiligte Kinder.",
+        "Webseite": "canchanabury.de",
+        "Nachricht": "Frohe Weihnachten, mein Kuscheltiger! 🎄"
+    },
+    {
+        "Tag": "23",
+        "Keyword": "ZIEGENHAUS",
+        "Name": "AYUDAME-Arequipa",
+        "Beschreibung": "Betreibt eine Kindertagesstätte in Peru und unterstützt benachteiligte Kinder und Familien mit Bildungs- und Versorgungsangeboten.",
+        "Webseite": "ayudame.de",
+        "Nachricht": "Erinnerst Du Dich an das Ziegenhaus in den Bergen? ⛰️"
+    },
+    {
+        "Tag": "9",
+        "Keyword": "5STERNEDELUXE",
+        "Name": "Bergwaldprojekt e.V.",
+        "Beschreibung": "Engagiert sich für den Schutz und Erhalt der Wälder in Deutschland und bietet Freiwilligenprojekte zur Aufforstung und Waldpflege.",
+        "Webseite": "bergwaldprojekt.de",
+        "Nachricht": "5 Sterne für die beste Ehefrau! ⭐⭐⭐⭐⭐"
+    },
+    {
+        "Tag": "21",
+        "Keyword": "CARWASH",
+        "Name": "Kinderheim Pauline",
+        "Beschreibung": "Bietet Schutz und Betreuung für Kinder in schwierigen Lebenssituationen in Niedersachsen mit Fokus auf Bildung und soziale Unterstützung.",
+        "Webseite": "kinderheim-pauline.de",
+        "Nachricht": "Unsere Carwash-Dates sind immer noch die besten! 🚗"
+    },
+    {
+        "Tag": "22",
+        "Keyword": "SCHÖNSCHARF",
+        "Name": "Verein für Berliner Stadtmission",
+        "Beschreibung": "Hilft obdachlosen und bedürftigen Menschen in Berlin durch Essensausgaben, Kältenotübernachtungen und Beratungsangebote.",
+        "Webseite": "berliner-stadtmission.de",
+        "Nachricht": "Schön und scharf - das bist Du! 🌶️"
+    },
+    {
+        "Tag": "16",
+        "Keyword": "SILVESTER",
+        "Name": "Obdachlosenhilfe Rostock gGmbH",
+        "Beschreibung": "Unterstützt Obdachlose in Rostock mit Unterkünften, Mahlzeiten und sozialer Beratung.",
+        "Webseite": "obdachlosenhilfe-rostock.de",
+        "Nachricht": "Unser erstes Silvester zusammen - unvergesslich! 🎆"
+    },
+    {
+        "Tag": "13",
+        "Keyword": "GROBI",
+        "Name": "CaFée mit Herz e.V.",
+        "Beschreibung": "Ein Tageszentrum in Hamburg, das Obdachlosen und Bedürftigen warme Mahlzeiten, Kleidung und medizinische Hilfe bietet.",
+        "Webseite": "cafeemitherz.de",
+        "Nachricht": "Mein kleiner Grobi, Du verzauberst jeden Tag! 💫"
+    },
+    {
+        "Tag": "6",
+        "Keyword": "ICEICEBABY",
+        "Name": "Kältebus München e.V.",
+        "Beschreibung": "Hilft obdachlosen Menschen in München bei eisigen Temperaturen mit warmen Getränken, Decken und Übernachtungsmöglichkeiten.",
+        "Webseite": "kaeltebus-muenchen.de",
+        "Nachricht": "Ice Ice Baby - unser Song! 🎵"
+    },
+    {
+        "Tag": "19",
+        "Keyword": "WALCHENSEE",
+        "Name": "Kältebus Saarbrücken e.V.",
+        "Beschreibung": "Unterstützt obdachlose Menschen im Raum Saarbrücken bei kalten Temperaturen mit Hilfsgütern und Übernachtungsmöglichkeiten.",
+        "Webseite": "kaeltebussaarbruecken.de",
+        "Nachricht": "Der Walchensee - unser Lieblingsort! 🏊‍♀️"
+    },
+    {
+        "Tag": "5",
+        "Keyword": "MAUSEBÄR",
+        "Name": "Elisabethstift",
+        "Beschreibung": "Bietet Betreuung, Bildung und Schutz für Kinder und Jugendliche in Notlagen, insbesondere in Berlin und Brandenburg.",
+        "Webseite": "elisabethstift-berlin.de",
+        "Nachricht": "Für meinen Mausebär - ich liebe Dich! 🐻"
+    },
+    {
+        "Tag": "17",
+        "Keyword": "DINOFAN",
+        "Name": "",
+        "Beschreibung": "",
+        "Webseite": "",
+        "Nachricht": "Für meinen größten Dino-Fan! 🦕"
+    }
+];
